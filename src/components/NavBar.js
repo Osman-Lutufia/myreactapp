fc0f780aed02fe001eaeadd2c47fffa05f56f180
Osman-Lutufia <110116081+Osman-Lutufia@users.noreply.github.com>
2022-08-11 10:37:08 -0700

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <h1>welcome to shop</h1>
+          <nav>
+              <div>
+                  <ul>
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>products</li>
+                      <li>Contact</li>
+                      
+                  </ul>
+
+              </div>
+          </nav>
+
+    </div>
+  )
+}
+
+export default NavBar
